5c10a457d0bf9d892e02620b31f20332b0692b07
Ross <ross@rossmasters.com>
2020-03-29 22:20:33 +0100

    Add favicons

--- web/src/index.js
+++ web/src/index.js
@@ -2,6 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CountriesPage from "./countries";
 
+import "./assets/android-chrome-192x192.png";
+import "./assets/android-chrome-512x512.png";
+import "./assets/apple-touch-icon.png";
+import "./assets/favicon-16x16.png";
+import "./assets/favicon-32x32.png";
+import "./assets/favicon.ico";
+import "./assets/site.webmanifest";
+
 ReactDOM.render(
   <CountriesPage />,
   document.getElementById('root')
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -19,6 +19,13 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|ico|webmanifest)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
       }
     ]
   },
